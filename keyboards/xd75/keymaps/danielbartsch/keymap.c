2b5ab39cbfbc9ea5f4336100d6bc5676950ad761
danielbartsch <daniel.bartsch@live.at>
2018-04-30 13:15:15 +0200

    cleaned up layout and just readded some keys

--- keyboards/xd75/keymaps/danielbartsch/keymap.c
+++ keyboards/xd75/keymaps/danielbartsch/keymap.c
@@ -19,54 +19,58 @@
 #define _WM 0
 #define _FN 1
 
+// key shorthand
+#define __ss__ KC_SPC
+#define ______ KC_TRNS
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* WORKMAN
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | ESC    | 0      | 1      | 2      | 3      | 4      | -      | DEL    | =      | 5      | 6      | 7      | 8      | 9      | BACKSP |
+ * | ESC    | 0      | 1      | 2      | 3      | 4      | HOME   | DEL    | END    | 5      | 6      | 7      | 8      | 9      |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB    | [ {    | Q      | D      | R      | W      | B      | BACKSP | J      | F      | U      | P      | ;      | ] }    | '      |
+ * |        |        | Q      | D      | R      | W      | B      | BACKSP | J      | F      | U      | P      | ;      |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------+--------|
- * | CAP LK | (      | A      | S      | H      | T      | G      | Enter  | Y      | N      | E      | O      | I      | )      | ENTER  |
+ * |        |        | A      | S      | H      | T      | G      | Enter  | Y      | N      | E      | O      | I      |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------+--------|
- * | LSHIFT | END    | Z      | X      | M      | C      | V      | UP     | K      | L      | ,      | .      | /      | PG DN  | RSHIFT |
+ * |        |        | Z      | X      | M      | C      | V      | UP     | K      | L      | ,      | .      | /      |        |        |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | LGUI   | LALT   | FN     | LSHIFT | SPACE  | LEFT   | DOWN   | RIGHT  | SPACE  | SPACE  | FN     | RALT   | RGUI   | RCTRL  |
+ * | LCTRL  | LGUI   | LALT   |        | LSHIFT | SPACE  | LEFT   | DOWN   | RIGHT  | SPACE  | FN     |        |        |        |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
 [_WM] = { /* Workman */
-  { KC_ESC,  KC_0,    KC_1,    KC_2,    KC_3,    KC_4,   KC_MINS, KC_DEL,  KC_EQL,  KC_5, KC_6,    KC_7,    KC_8,    KC_9,    KC_BSPC },
-  { KC_TAB,  KC_LBRC, KC_Q,    KC_D,    KC_R,    KC_W,   KC_B,   KC_BSPC, KC_J,   KC_F,   KC_U,    KC_P,    KC_SCLN, KC_RBRC, KC_QUOT },
-  { KC_CAPS, KC_LPRN, KC_A,    KC_S,    KC_H,    KC_T,   KC_G,   KC_ENT,  KC_Y,   KC_N,   KC_E,    KC_O,    KC_I,    KC_RPRN, KC_ENT  },
-  { KC_LSFT, KC_END,  KC_Z,    KC_X,    KC_M,    KC_C,   KC_V,   KC_UP,   KC_K,   KC_L,   KC_COMM, KC_DOT,  KC_SLSH, KC_PGDN, KC_RSFT },
-  { KC_LCTL, KC_LGUI, KC_LALT, MO(_FN), KC_LSFT, KC_SPC, KC_LEFT,KC_DOWN, KC_RGHT, KC_SPC,KC_SPC,  MO(_FN), KC_RALT, KC_RGUI, KC_RCTL },
+  { KC_ESC,  KC_0,    KC_1,    KC_2,   KC_3,    KC_4,   KC_HOME,KC_DEL,  KC_END,  KC_5,   KC_6,    KC_7,   KC_8,    KC_9,   __ss__ },
+  { __ss__,  __ss__,  KC_Q,    KC_D,   KC_R,    KC_W,   KC_B,   KC_BSPC, KC_J,    KC_F,   KC_U,    KC_P,   KC_SCLN, __ss__, __ss__ },
+  { __ss__ , __ss__,  KC_A,    KC_S,   KC_H,    KC_T,   KC_G,   KC_ENT,  KC_Y,    KC_N,   KC_E,    KC_O,   KC_I,    __ss__, __ss__ },
+  { __ss__ , __ss__,  KC_Z,    KC_X,   KC_M,    KC_C,   KC_V,   KC_UP,   KC_K,    KC_L,   KC_COMM, KC_DOT, KC_SLSH, __ss__, __ss__ },
+  { KC_LCTL, KC_LGUI, KC_LALT, __ss__, KC_LSFT, __ss__, KC_LEFT,KC_DOWN, KC_RGHT, __ss__, MO(_FN), __ss__, __ss__,  __ss__, __ss__ },
  },
 
 /* FUNCTION
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | F1     | F2     | F3     | F4     | F5     | F6     | NUM LK | P/     | P*     | F7     | F8     | F9     | F10    | F11    | F12    |
+ * | F1     | F2     | F3     | F4     | F5     | F6     |        |        |        | F7     | F8     | F9     | F10    | F11    | F12    |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | SELECT | CALC   | MYCOMP | MAIL   |        |        | P7     | P8     | P9     | -      |        |        | PR SCR | SCR LK | PR SCR |
+ * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | PREV   | PLAY   | NEXT   | STOP   |        |        | P4     | P5     | P6     | +      |        |        |        | RGB_TOG|        |
+ * |        |        |        | {      | [      | (      |        |        |        | )      | ]      | }      | =      | RGB_TOG|        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | VOL-   | MUTE   | VOL+   |        |        |        | P1     | P2     | P3     | PENT   |        |        | HUI    | SAI    | VAI    |
+ * |        |        |        |        |        | -      |        |        |        | +      |        |        | HUI    | SAI    | VAI    |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | RESET  |        | RESET  | FN     |        |        | P0     |        | P.     | PENT   | PENT   | FN     | HUD    | SAD    | VAD    |
+ * |        |        | RESET  | FN     |        | TAB    |        |        |        |        |        | FN     | HUD    | SAD    | VAD    |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
  
  [_FN] = { /* FUNCTION */
-  { KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,  KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 },
-  { KC_MSEL, KC_CALC, KC_TRNS, KC_MAIL, KC_SPC, KC_SPC, KC_P7,   KC_P8,   KC_P9,   KC_MINS, KC_SPC,  KC_SPC,  KC_TRNS, KC_TRNS, KC_TRNS },
-  { KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, KC_SPC, KC_SPC, KC_P6,   KC_P5,   KC_P4,   KC_PLUS, KC_SPC,  KC_TRNS, KC_SPC,  RGB_TOG, KC_SPC },
-  { KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  KC_SPC, KC_SPC, KC_P3,   KC_P2,   KC_P1,   KC_TRNS, KC_SPC,  KC_SPC,  RGB_HUI, RGB_SAI, RGB_VAI },
-  { RESET,   KC_TRNS, RESET  , MO(_FN), KC_SPC, KC_SPC, KC_KP_0, KC_TRNS, KC_PDOT, KC_PENT, KC_TRNS, MO(_FN), RGB_HUD, RGB_SAD, RGB_VAD },
+  { KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,  ______, ______, ______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12  },
+  { ______,  ______,  ______,  ______,  ______,  ______, ______, ______, ______, ______,  ______,  ______,  ______,  ______,  ______  },
+  { ______, ______,   ______,  KC_LCBR, KC_LBRC, KC_LPRN,______, ______, ______, KC_RPRN, KC_RBRC, KC_RCBR, KC_EQL,  RGB_TOG, ______  },
+  { ______, ______,   ______,  ______,  ______,  KC_PMNS,______, ______, ______, KC_PPLS, ______,  ______,  RGB_HUI, RGB_SAI, RGB_VAI },
+  { ______, ______,   RESET,   MO(_FN), ______,  KC_TAB, ______, ______, ______, ______,  ______,  MO(_FN), RGB_HUD, RGB_SAD, RGB_VAD },
  }
 };
 
-const uint16_t PROGMEM fn_actions[] = {
+const uint16_t PROGMEM fn_actions[] = { 
 
 };
 

fbc63c79ffa2ccf218ef2ecfd4013871816243eb
danielbartsch <daniel.bartsch@live.at>
2018-05-04 22:38:08 +0200

    added shift layer, and prepared unicode input

--- keyboards/xd75/keymaps/danielbartsch/keymap.c
+++ keyboards/xd75/keymaps/danielbartsch/keymap.c
@@ -17,56 +17,93 @@
 
 // Layer shorthand
 #define _WM 0
-#define _FN 1
+#define _SH 1
+#define _FN 2
+
+// os switches
+// #define OS_LIN 101
+// #define OS_MAC 100
+// #define OS_WIN 99
+
+// unicode
+// #define umA 0x00C4
+// #define uma 0x00E4
+// #define umO 0x00D6
+// #define umo 0x00F6
+// #define umU 0x00DC
+// #define umu 0x00FC
+// #define umS 0x1E9E
+// #define ums 0x00DF
 
 // key shorthand
 #define __ss__ KC_SPC
 #define ______ KC_TRNS
-// lsft = s = left shift + keycode
-// sft_t = left shift when held, parameter when tapped 
+// rsft = tap letter with right shift
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* WORKMAN
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | ESC    | 0      | 1      | 2      | 3      | 4      | HOME   | DEL    | END    | 5      | 6      | 7      | 8      | 9      |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * |        |        | Q q    | D d    | R r    | W w    | B b    | BACKSP | J j    | F f    | U u    | P p    | ; :    |        |        |
+ * |        | CAPS   | q      | d      | r      | w      | b      | BACKSP | j      | f      | u      | p      | ;      |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------+--------|
- * |        | TAB    | A a    | S s    | H h    | T t    | G g    | Enter  | Y y    | N n    | E e    | O o    | I i    |        |        |
+ * |        | TAB    | a      | s      | h      | t      | g      | Enter  | y      | n      | e      | o      | i      |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------+--------|
- * |        |        | Z z    | X x    | M m    | C c    | V v    | UP     | K k    | L l    | , <    | . >    | / ?    |        |        |
+ * |        |        | z      | x      | m      | c      | v      | UP     | k      | l      | ,      | .      | /      |        |        |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | LGUI   | LALT   |        |        | SPACELS| LEFT   | DOWN   | RIGHT  | SPACEFN|        |        |        |        |        |
+ * | LGUI   |        |        |        | LCTRL  | SPACELS| LEFT   | DOWN   | RIGHT  | SPACEFN| RALT   |        |        |        |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
 [_WM] = { /* WORKMAN */
-  { KC_ESC,  KC_0,    KC_1,    KC_2,   KC_3,   KC_4,          KC_HOME,KC_DEL,  KC_END,  KC_5,            KC_6,    KC_7,   KC_8,    KC_9,   __ss__ },
-  { __ss__,  __ss__,  KC_Q,    KC_D,   KC_R,   KC_W,          KC_B,   KC_BSPC, KC_J,    KC_F,            KC_U,    KC_P,   KC_SCLN, __ss__, __ss__ },
-  { __ss__ , KC_TAB,  KC_A,    KC_S,   KC_H,   KC_T,          KC_G,   KC_ENT,  KC_Y,    KC_N,            KC_E,    KC_O,   KC_I,    __ss__, __ss__ },
-  { __ss__ , __ss__,  KC_Z,    KC_X,   KC_M,   KC_C,          KC_V,   KC_UP,   KC_K,    KC_L,            KC_COMM, KC_DOT, KC_SLSH, __ss__, __ss__ },
-  { KC_LCTL, KC_LGUI, KC_LALT, __ss__, __ss__, SFT_T(__ss__), KC_LEFT,KC_DOWN, KC_RGHT, LT(_FN, __ss__), __ss__,  __ss__, __ss__,  __ss__, __ss__ },
+  { KC_ESC, KC_0,    KC_1,   KC_2,   KC_3,     KC_4,            KC_HOME,KC_DEL,  KC_END,  KC_5,            KC_6,    KC_7,   KC_8,    KC_9,   __ss__ },
+  { __ss__, KC_CAPS, KC_Q,   KC_D,   KC_R,     KC_W,            KC_B,   KC_BSPC, KC_J,    KC_F,            KC_U,    KC_P,   KC_SCLN, __ss__, __ss__ },
+  { __ss__, KC_TAB,  KC_A,   KC_S,   KC_H,     KC_T,            KC_G,   KC_ENT,  KC_Y,    KC_N,            KC_E,    KC_O,   KC_I,    __ss__, __ss__ },
+  { __ss__, __ss__,  KC_Z,   KC_X,   KC_M,     KC_C,            KC_V,   KC_UP,   KC_K,    KC_L,            KC_COMM, KC_DOT, KC_SLSH, __ss__, __ss__ },
+  { KC_LGUI,__ss__,  __ss__, __ss__, KC_LCTRL, LT(_SH, __ss__), KC_LEFT,KC_DOWN, KC_RGHT, LT(_FN, __ss__), KC_RALT,  __ss__, __ss__,  __ss__, __ss__ },
+ },
+
+ /* SHIFT
+ * .--------------------------------------------------------------------------------------------------------------------------------------.
+ * |        |        |        |        |        |        | HOME   |        | END    |        |        |        |        |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
+ * |        |        | Q      | D      | R      | W      | B      |        | J      | F      | U      | P      | :      |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------+--------|
+ * |        | TAB    | A      | S      | H      | T      | G      |        | Y      | N      | E      | O      | I      |        |        |
+ * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------+--------|
+ * |        |        | Z      | X      | M      | C      | V      | UP     | K      | L      | ?      | !      | \      |        |        |
+ * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
+ * |        |        |        |        |        |        | LEFT   | DOWN   | RIGHT  |        |        |        |        |        |        |
+ * '--------------------------------------------------------------------------------------------------------------------------------------'
+ */
+
+[_SH] = { /* SHIFT */
+  { ______, ______, ______,     ______,     ______,     ______,     RSFT(KC_HOME), ______,        RSFT(KC_END),  ______,     ______,      ______,     ______,     ______, ______ },
+  { ______, ______, RSFT(KC_Q), RSFT(KC_D), RSFT(KC_R), RSFT(KC_W), RSFT(KC_B),    ______,        RSFT(KC_J),    RSFT(KC_F), RSFT(KC_U),  RSFT(KC_P), KC_COLN,    ______, ______ },
+  { ______, ______, RSFT(KC_A), RSFT(KC_S), RSFT(KC_H), RSFT(KC_T), RSFT(KC_G),    ______,        RSFT(KC_Y),    RSFT(KC_N), RSFT(KC_E),  RSFT(KC_O), RSFT(KC_I), ______, ______ },
+  { ______, ______, RSFT(KC_Z), RSFT(KC_X), RSFT(KC_M), RSFT(KC_C), RSFT(KC_V),    RSFT(KC_UP),   RSFT(KC_K),    RSFT(KC_L), KC_QUES,     KC_EXLM,    KC_BSLASH,  ______, ______ },
+  { ______, ______, ______,     ______,    ______,     ______,     RSFT(KC_LEFT), RSFT(KC_DOWN), RSFT(KC_RGHT), ______,     ______,      ______,     ______,     ______, ______ },
  },
 
 /* FUNCTION
  * .--------------------------------------------------------------------------------------------------------------------------------------.
  * | F1     | F2     | F3     | F4     | F5     | F6     |        |        |        | F7     | F8     | F9     | F10    | F11    | F12    |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * | VOLDWN | VOLUP  |        |        |        | _      |        |        |        | *      |        |        |        |        |        |
+ * | VOLDWN | VOLUP  |        | @      | _      | &      | ^      |        | `      | %      | *      | =      |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        |        |        | {      | [      | (      |        |        |        | )      | ]      | }      | =      |        |        |
+ * |        |        | <      | {      | [      | (      | #      |        | "      | )      | ]      | }      | >      |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        |        |        |        |        | -      |        | PGDOWN |        | +      |        |        | !      |        |        |
+ * |        |        |        |        |        | -      | $      | PGUP   | '      | +      |        |        | |      |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        |        |        |        |        |        |        | PGUP   | RESET  | FN     |        |        |        |        |        |
+ * |        |        |        |        |        |        |        | PGDOWN | RESET  | FN     |        |        |        |        |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
- 
+
  [_FN] = { /* FUNCTION */
   { KC_F1,             KC_F2,           KC_F3,  KC_F4,   KC_F5,   KC_F6,  ______, ______,    ______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12 },
-  { KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, ______, ______,  ______,  KC_UNDS,______, ______,    ______, S(KC_8), ______,  ______,  ______,  ______, ______ },
-  { ______,            ______,          ______, KC_LCBR, KC_LBRC, KC_LPRN,______, ______,    ______, KC_RPRN, KC_RBRC, KC_RCBR, KC_EQL,  ______, ______ },
-  { ______,            ______,          ______, ______,  ______,  KC_PMNS,______, KC_PGUP,   ______, KC_PPLS, ______,  ______,  S(KC_1), ______, ______ },
+  { KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, ______, KC_AT,   KC_UNDS, KC_AMPR,KC_CIRC,______,    KC_GRV, ______,  KC_ASTR, KC_EQL,  ______,  ______, ______ },
+  { ______,            ______,          KC_LT,  KC_LCBR, KC_LBRC, KC_LPRN,KC_HASH,______,    KC_DQT, KC_RPRN, KC_RBRC, KC_RCBR, KC_GT,   ______, ______ },
+  { ______,            ______,          ______, ______,  ______,  KC_PMNS,KC_DLR, KC_PGUP,   KC_QUOT,KC_PPLS, ______,  ______,  KC_PIPE, ______, ______ },
   { ______,            ______,          ______, ______,  ______,  ______, ______, KC_PGDOWN, RESET,  ______,  ______,  ______,  ______,  ______, ______ },
  }
 };
@@ -92,3 +129,34 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
       }
     return MACRO_NONE;
 };
+
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+//   switch (keycode) {
+//     /* os switcher */
+//     case OS_LIN:
+//       set_unicode_input_mode(UC_LNX);
+//       rgblight_enable();
+//       rgblight_setrgb(233, 84, 32);
+//       return false;
+//       break;
+//     case OS_WIN:
+//       set_unicode_input_mode(UC_WINC);
+//       rgblight_enable();
+//       rgblight_setrgb(0, 122, 204);
+//       return false;
+//       break;
+//     case OS_MAC:
+//       set_unicode_input_mode(UC_OSX);
+//       rgblight_enable();
+//       rgblight_setrgb(255, 255, 255);
+//       return false;
+//       break;
+//   }
+//   return true;
+// }
+
+// void matrix_init_user() {
+//   set_unicode_input_mode(UC_WINC);
+//   rgblight_enable();
+//   rgblight_setrgb(0, 122, 204);
+// }
